573533671b8181e0ab0e20f143ec526b28bd77de
Emma Machacova <emmachac@gmail.com>
2019-10-29 16:18:53 +0100

    add praca so subormi

---
+++ matfyz/programovanie/3. domace zadanie/riesenie.py
@@ -0,0 +1,10 @@
+# 2. zadanie: zarovnaj
+# autor: Mario Cheben
+# datum: 30.10.2019
+
+# Napíš pythonovský skript, ktorý bude definovať túto funkciu:
+# vypis (meno_suboru, sirka, zarovnat=True, odsek=-1)
+
+with open("subor1.txt") as f:
+    for line in f:
+        print (line.strip())
