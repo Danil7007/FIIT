a38c09560dbc4dc84692ac6b37aa5f2f923a2d9d
Emma Machacova <emmachac@gmail.com>
2020-03-25 16:39:03 +0100

    Update zadanie02_machacova.c

--- LS_2019_2020/SEMAP/zadania/zadanie02/zadanie02_machacova.c
+++ LS_2019_2020/SEMAP/zadania/zadanie02/zadanie02_machacova.c
@@ -3,8 +3,8 @@
 // N tak akurat na tri slova
 #define N 1500
 
-int nacitanie (char *pole){
-    int velkost, i=0, j=0, pocet=0;
+void nacitanie (char *pole, int *ind){
+    int velkost, i=0, j=0, k=0, pocet=0;
     char c;
     
     velkost = N + 1;
@@ -31,29 +31,57 @@ int nacitanie (char *pole){
             pocet++;
         }
     }
-    //printf("\n%d", pocet);
     
-    return pocet;
+    // realokovanie pre zisteny pocet slov (*2 lebo zaciatok-koniec a +1 lebo velkost)
+    ind = (int *)realloc(ind, (2*pocet)+1);
+    
+    // zapis indexov
+    // na zaciatku pola dlzka
+    *(ind+k) = (2*pocet);
+    k++;
+    // prve slovo vzdy na indexe 0
+    *(ind+k) = 0;
+    k++;
+    for (j=0; j<i; j++){
+        if(*(pole+j) == '\n'){
+            *(ind+k) = j;
+            k++;
+            // zaciatok dalsieho slova
+            *(ind+k) = j+1;
+            k++;
+        }
+    }
+    // posledny enter
+    *(ind+k) = i;
 }
 
 // vypisovanie vstupu - hotovo
-void vypis (char *pole){
-    int i=0;
+void vypis (char *pole, int *ind){
+    int i=0, dlzka;
+    
     while (*(pole+i) != '\0'){
         printf("%c", *(pole+i));
         i++;
+    }
+    
+    dlzka = *ind;
+    for (i=1; i <= dlzka; i++){
+        printf("%d ", *(ind+i));
     }
 }
 
 int main(){
     char *slova;
-    int velkost;
+    int velkost, *indexy;
     velkost = N + 1;
     
+    // pole pre slova zo vstupu
     slova = (char *)malloc(velkost *sizeof(char));
+    // pole s indexami zaciatkov a koncov slov
+    indexy = (int *)malloc(6 *sizeof(int));
     
-    nacitanie(slova);
-    vypis(slova);
+    nacitanie(slova, indexy);
+    vypis(slova, indexy);
     
     return 0;
 }
