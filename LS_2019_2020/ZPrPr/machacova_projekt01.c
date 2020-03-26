7d79b4a8766595bbc0983bdc703fc55fd7ec056d
Emma Machacova <emmachac@gmail.com>
2020-03-26 17:04:22 +0100

    Update machacova_projekt01.c

--- LS_2019_2020/ZPrPr/machacova_projekt01.c
+++ LS_2019_2020/ZPrPr/machacova_projekt01.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #define N 20
 
 char **pole;
+char **maska;
 int **znaky;
 int pocty[26], dlzky[26];
+FILE * subor; 
 
 void nacitaj(int *vyska, int *sirka){
-    FILE * subor = fopen("osemsmerovka.txt", "r");
     int i=0, r=0, s=0;
     char c;
     
@@ -19,6 +21,13 @@ void nacitaj(int *vyska, int *sirka){
     // alokovanie miesta pre pole
     for (i=0; i < *vyska; i++){
         pole[i] = (char *)malloc(*sirka+1 *sizeof(char)); // +1 pre nulovy znak
+    }
+    
+    // maska
+    maska = (char **)malloc(*vyska *sizeof(char *));
+    // alokovanie miesta pre pole
+    for (i=0; i < *vyska; i++){
+        maska[i] = (char *)calloc(*sirka+1, sizeof(char)); // +1 pre nulovy znak
     }
     
     // nacitanie po riadkoch zo suboru
@@ -58,14 +67,21 @@ void nacitaj(int *vyska, int *sirka){
 
 // hotova funkcia 
 void vypis(int *vyska, int *sirka){
-    int r, s;
-
+    int r, s, rozdiel = 'a' - 'A';
+    
+    // osemsmerovka
     for(r=0; r < *vyska; r++){
-        printf("%s\n", pole[r]);
+        for (s=0; s < *sirka; s++){
+            if (maska[r][s] == 0)
+                printf("%c", pole[r][s]);   
+            else printf("%c", pole[r][s] + rozdiel);
+        }
+        printf("\n");
     }
     
     printf("\n");
-    
+    /*
+    // tabulka znakov
     for (r=0; r < 26; r++){
         printf("%c: ", r+'A');
         for (s=0; s < pocty[r]*2; s++){
@@ -74,20 +90,92 @@ void vypis(int *vyska, int *sirka){
         printf("\n");
     }
     printf("\n");
+    */
 }
 
-void riesenie(){
+void riesenie(int *vyska, int *sirka){
     int zmena_r[8] = {-1, -1, 0, 1, 1, 1, 0, -1};
     int zmena_s[8] = {0, 1, 1, 1, 0, -1, -1, -1};
+    int dlzka, i, j, k, z, nasiel, zs, zr, smer, s, r;
+    char *slovo = (char*)malloc(sizeof(char));
     
+    if (*sirka >= *vyska){
+        slovo = (char*)malloc((*sirka+1) *sizeof(char));
+    }
+    else {
+        slovo = (char*)malloc((*vyska+1) *sizeof(char));
+    }
+    
+    // nove / dalsie slovo
+    printf("zacinam\n");
+    while (fscanf(subor, "%s", slovo) > 0){
+        i = slovo[0] - 'A';
+        nasiel = 0;
+        dlzka = strlen (slovo);
+        printf("%s %d %d\n", slovo, i, dlzka);
+        
+        // rozne zaciatky
+        for (z=0; z < pocty[i]; z++){
+            // zaciatocne indexy do osemsmerovky pre prve pismeno pre hadane slovo
+            zr = znaky[i][z*2];
+            zs = znaky[i][z*2 + 1];  
+            
+            // rozne smery 
+            for (smer = 0; smer < 8; smer++){
+                s = zs;
+                r = zr;
+                j = 0;
+                
+                // porovnavanie pismen slova s osemsmerovkou
+                while ((j < dlzka) && (s >= 0) && (s < *sirka) && (r >= 0) && (r < *vyska)){
+                    printf("zr %d zs %d r %d s %d j %d smer %d\n", zr, zs, r, s, j, smer);
+                    
+                    // ak sa nezhoduje
+                    if (slovo[j] != pole[r][s]){
+                        break;
+                    }
+                    
+                    // ak sa zhoduje 
+                    if (j >= dlzka - 1){
+                        nasiel = 1;
+                        printf("NASIEL!!\n");
+                        
+                        // zmena na male pismenka 
+                        for (k = 0, s = zs, r = zr;    k < dlzka;    k++, s += zmena_s[smer], r += zmena_r[smer]){
+                            maska[r][s] = 1;
+                        }
+                        
+                        break;
+                    }
+                    
+                    // inkrementacia
+                    j++;
+                    s += zmena_s[smer];
+                    r += zmena_r[smer];
+                }
+                
+                if (nasiel == 1) break;
+            }
+            
+            if (nasiel == 1) break;
+        }
+        
+        if (nasiel == 1) vypis(vyska, sirka);
+        else printf("slovo nebolo najdene\n");
+    }
 }
 
 int main(){
     int m, n, spolu, *pm = &m, *pn = &n;
     int i=0;
+    subor = fopen("osemsmerovka.txt", "r");
     
     nacitaj(pm, pn);
+    printf("...\n");
     vypis(pm, pn);
-
+    printf("...\n");
+    riesenie(pm, pn);
+    printf("...\n");
+    
     return 0;
 }
